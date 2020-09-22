eab85e5bf201a3481879638de6d587bdd50e943d
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-09-22 15:40:06 -0400

    Created product category model/table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_193654) do
+ActiveRecord::Schema.define(version: 2020_09_22_193855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "store_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["store_id"], name: "index_product_categories_on_store_id"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
@@ -58,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_09_22_193654) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "product_categories", "stores"
   add_foreign_key "reviews", "stores"
   add_foreign_key "reviews", "users"
   add_foreign_key "stores", "users"
