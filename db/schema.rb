d2316096b479a2d367dde93044dc46b9a7e5942a
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-09-22 15:38:04 -0400

    Created reviews model/table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_193441) do
+ActiveRecord::Schema.define(version: 2020_09_22_193654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "store_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["store_id"], name: "index_reviews_on_store_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
@@ -47,5 +58,7 @@ ActiveRecord::Schema.define(version: 2020_09_22_193441) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "stores"
+  add_foreign_key "reviews", "users"
   add_foreign_key "stores", "users"
 end
